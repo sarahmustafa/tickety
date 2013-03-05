b8e1a28c60f34a12c107bfc2fb659893d10b1174
jaasimpolin <jaasimpolin@gmail.com>
2013-03-05 01:41:45 -0800

    new

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,10 +1,7 @@
 class Theater < ActiveRecord::Base
-<<<<<<< HEAD
-   attr_accessible :cinema_id, :theater_number
-=======
-  attr_accessible :cinema_id, :theater_number
+attr_accessible :cinema_id, :theater_number
+attr_accessible :cinema_id, :theater_number
 has_many :shows
 has_many :seats
 belongs_to :cinema
->>>>>>> upstream/master
 end
